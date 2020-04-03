b4b02b013ee8c614fcab5f55eabfaa2882011173
TimonC <58507497+TimonC@users.noreply.github.com>
2020-04-03 12:05:53 +0200

    Unit test veranderingen met gebruik van andere methodes en public variabelen

--- CircustreinTests/TrainTests.cs
+++ CircustreinTests/TrainTests.cs
@@ -11,50 +11,40 @@ namespace Circustrein.Tests
     [TestClass()]
     public class TrainTests
     {
+        public int count;
+        public bool succes;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
         [TestMethod()]
         public void DivideAnimalsTest()
         {
             //Check of beide dieren worden gebruikt ondanks dat herbivoor/carnivoor zijn.
-            int count = 0;
             Animal leeuw = new Animal("Leeuw", Animal.Diet.Carnivoor, Animal.Points.Middel);
             Animal olifant = new Animal("Olifant", Animal.Diet.Herbivoor, Animal.Points.Groot);
-            List<Animal> animals = new List<Animal>();
-            animals.Add(leeuw);
-            animals.Add(olifant);
             Train train = new Train();
-            foreach (Animal animal in animals)
+            train.Animals.Add(leeuw);
+            train.Animals.Add(olifant);
+            foreach (Animal animal in train.Animals)
             {
                 train.DivideAnimals(animal);
-                if (animal.used == true)
+                if (animal.used)
                 {
                     count++;
                 }
-                else
-                {
-                }
             }
-
             Assert.AreEqual(2, count);
-
         }
 
         [TestMethod()]
         public void WagonCheckTest()
         {
-        //Check als het dier wordt geparsed en er een nieuwe wagon wordt aangemaakt.
-            bool succes;
+        //Check als het dier wordt geparsed en er een nieuwe wagon wordt aangemaakt. 
             Animal olifant = new Animal("Olifant", Animal.Diet.Herbivoor, Animal.Points.Groot);
             Train train = new Train();
             train.WagonCheck(olifant);
-
-            if(train.Wagons.Any())
-            {
-                succes = true;
-            }
-            else
-            {
-                succes = false;
-            }
+            succes = train.Wagons.Any();
             Assert.AreEqual(true, succes);
         }
     }
--- CircustreinTests/WagonTests.cs
+++ CircustreinTests/WagonTests.cs
@@ -12,33 +12,26 @@ namespace Circustrein.Tests
     public class WagonTests
     {
         public bool succes;
-        List<Animal> animals;
-        List<Animal> animalsInWagon;
+
         [TestInitialize]
         public void TestInitialize()
         {
-            animals = new List<Animal>();
-            animalsInWagon = new List<Animal>();
         }
 
         [TestMethod()]
-        public void CheckCompatabilityTest()
+        public void CheckRulesHerbivorCheck()
         {
             //Check als het algoritme werkt door een olifant en leeuw toe te voegen. Dit lukt omdat de olifant een herbivoor en groter is dan de leeuw. 
             Wagon wagon = new Wagon(10);
-            bool succes;
             Animal leeuw = new Animal("Leeuw", Animal.Diet.Carnivoor, Animal.Points.Middel);
             Animal olifant = new Animal("Olifant", Animal.Diet.Herbivoor, Animal.Points.Groot);
             Animal aap = new Animal("Aap", Animal.Diet.Herbivoor, Animal.Points.Middel);
             Animal konijn = new Animal("Konijn", Animal.Diet.Herbivoor, Animal.Points.Klein);
             Animal zeehond = new Animal("Zeehond", Animal.Diet.Carnivoor, Animal.Points.Middel);
             Animal parakiet = new Animal("Parakiet", Animal.Diet.Herbivoor, Animal.Points.Klein);
-            animals.Add(olifant);
-            animalsInWagon.Add(leeuw);
+            wagon.AddAnimal(leeuw);
             succes = wagon.CheckRulesHerbivor(olifant);
             Assert.AreEqual(true, succes);
-
-
         }
 
 
@@ -46,7 +39,6 @@ namespace Circustrein.Tests
         public void CheckPointsTest()
         {
             //Check als de punten goed van de capacity van de wagon worden afgetrokken. (Er zit al een olifant in dus er zijn 5 resterende punten).
-            bool succes;
             Wagon wagon = new Wagon(10);
             Animal leeuw = new Animal("Leeuw", Animal.Diet.Carnivoor, Animal.Points.Middel);
             Animal olifant = new Animal("Olifant", Animal.Diet.Herbivoor, Animal.Points.Groot);
@@ -54,11 +46,10 @@ namespace Circustrein.Tests
             Animal konijn = new Animal("Konijn", Animal.Diet.Herbivoor, Animal.Points.Klein);
             Animal zeehond = new Animal("Zeehond", Animal.Diet.Carnivoor, Animal.Points.Middel);
             Animal parakiet = new Animal("Parakiet", Animal.Diet.Herbivoor, Animal.Points.Klein);
-            animalsInWagon.Add(aap); //3
-            animalsInWagon.Add(konijn); //1
-            animalsInWagon.Add(leeuw); //3
-            animalsInWagon.Add(olifant); //5
-            wagon.capacity -= Convert.ToInt32(olifant.points);
+            //wagon.AddAnimal(aap); //3 punten
+            //wagon.AddAnimal(konijn); //1 punt
+            //wagon.AddAnimal(leeuw); //3 punten
+            wagon.AddAnimal(olifant); //5 punten
             succes = wagon.CheckPointsHerbivor(olifant);
             Assert.AreEqual(true, succes);
 
